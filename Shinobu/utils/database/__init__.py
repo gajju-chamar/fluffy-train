0279e11dc0691d8946650ec7b17ea05eef7428ef
gajju-chamar <dicompeswari@gmail.com>
2026-06-14 11:07:51 +0530

    Update __init__.py

--- Shinobu/utils/database/__init__.py
+++ Shinobu/utils/database/__init__.py
@@ -5,3 +5,7 @@ from .approveddatabase import *
 from .assistantdatabase import *
 from .memorydatabase import *
 from .mongodatabase import *
+
+# Cheat Code Bypass for missing private chat function
+async def is_served_private_chat(chat_id: int) -> bool:
+    return True
