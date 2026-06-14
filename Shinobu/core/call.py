69451ca29b70e06d8ddef4b21a7f3d8a727e6706
gajju-chamar <dicompeswari@gmail.com>
2026-06-14 11:37:06 +0530

    Update call.py

--- Shinobu/core/call.py
+++ Shinobu/core/call.py
@@ -413,19 +413,6 @@ class Call(PyTgCalls):
             await self.five.start()
 
     async def decorators(self):
-        @self.one.on_closed_voice_chat()
-        @self.two.on_closed_voice_chat()
-        @self.three.on_closed_voice_chat()
-        @self.four.on_closed_voice_chat()
-        @self.five.on_closed_voice_chat()
-        @self.one.on_left()
-        @self.two.on_left()
-        @self.three.on_left()
-        @self.four.on_left()
-        @self.five.on_left()
-        async def stream_services_handler(_, chat_id: int):
-            await self.stop_stream(chat_id)
-
         @self.one.on_stream_end()
         @self.two.on_stream_end()
         @self.three.on_stream_end()
